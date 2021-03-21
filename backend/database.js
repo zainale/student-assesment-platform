83a9f4c17f4ce0f039237d6366fe100f0915f438
ZAIN <zainalework@gmail.com>
2021-03-21 17:02:14 +0500

    Added database.js

---
+++ backend/database.js
@@ -0,0 +1,56 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./database.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+});
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT,
+        role TEXT,
+        name TEXT,
+        email TEXT
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS sections (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        lecturer_id INTEGER
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS section_students (
+        section_id INTEGER,
+        student_id INTEGER
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        description TEXT,
+        expected_output TEXT,
+        lecturer_id INTEGER,
+        section_id INTEGER
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS submissions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task_id INTEGER,
+        student_id INTEGER,
+        code TEXT,
+        status TEXT,
+        grade INTEGER
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS feedbacks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        message TEXT,
+        date TEXT
+    )`);
+});
+
+module.exports = db;
